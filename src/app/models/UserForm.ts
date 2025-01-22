fe3a80d8679d04a97f43be1466fc1519fa543674
StasVasilenko567 <vasilenko.fortech@gmail.com>
2025-01-22 16:03:24 +0300

    Update UserForm.ts

--- src/app/models/UserForm.ts
+++ src/app/models/UserForm.ts
@@ -4,7 +4,7 @@ import { User } from "./User";
 export function GetFormBuilder(args?: User): FormGroup {
     return new FormGroup({
         "name": new FormControl(args?.name, [Validators.required]),
-        "username": new FormControl(args?.username, [Validators.required]),
+        "username": new FormControl(args?.username, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
         "email": new FormControl(args?.email, [Validators.required, Validators.email]),
         "phone": new FormControl(args?.phone, [Validators.required, Validators.pattern('[0-9]{10}')]),
     });
