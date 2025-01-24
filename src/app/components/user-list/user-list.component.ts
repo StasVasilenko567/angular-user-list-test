f030d3a107176c75bc491eaa9a12ed151f50e09a
StasVasilenko567 <vasilenko.fortech@gmail.com>
2025-01-24 14:27:16 +0300

    Add Error handling

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { AsyncPipe, NgFor } from '@angular/common';
 import { UserCardComponent } from '@components/user-card/user-card.component';
 import { userActions } from 'app/store/user.actions';
 import { Store } from '@ngrx/store';
 import { userSelectors } from 'app/store/user.selectors';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
@@ -11,11 +12,24 @@ import { userSelectors } from 'app/store/user.selectors';
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   private readonly store = inject(Store);
   public users$ = this.store.select(userSelectors.selectUsers);
-  
+
+  public isError$ = this.store.select(userSelectors.selectIsError);
+  private isErrorSubscription: Subscription | undefined;
+
   public ngOnInit(): void {
     this.store.dispatch(userActions.loadUsers());
+
+    this.isErrorSubscription = this.isError$.subscribe((isError) => {
+      if (isError) {
+        console.error('Ошибка загрузки пользователей');
+      }
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.isErrorSubscription?.unsubscribe();
   }
 }
--- src/app/store/user.selectors.ts
+++ src/app/store/user.selectors.ts
@@ -3,7 +3,11 @@ import { userFeatureKey, UserState } from "./user.reducers";
 
 const selectFeature = createFeatureSelector<UserState>(userFeatureKey);
 const selectUsers = createSelector(selectFeature, (state: UserState) => state.data.users);
+const selectIsLoading = createSelector(selectFeature, (state: UserState) => state.isLoading);
+const selectIsError = createSelector(selectFeature, (state: UserState) => state.isError);
 
 export const userSelectors = {
   selectUsers,
+  selectIsLoading,
+  selectIsError,
 };
