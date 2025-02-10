1f0b07df87b861e783c0984e26a65bc965631f33
StasVasilenko567 <vasilenko.fortech@gmail.com>
2025-02-10 15:04:55 +0300

    UnPied(

--- src/app/pages/charts-page/components/charts-list/chart-list.component.ts
+++ src/app/pages/charts-page/components/charts-list/chart-list.component.ts
@@ -3,15 +3,17 @@ import { HighchartsChartComponent, HighchartsChartModule } from 'highcharts-angu
 import * as Highcharts from 'highcharts';
 import { XmasComponent } from "../xmas-chart/xmas.component";
 import { PinesChartComponent } from "../pines-chart/pines-chart.component";
+import { PieChartComponent } from "../pie-chart/pie-chart.component";
 // import { ChartListComponent } from "../xmas-chart/xmas.component";
 
 @Component({
     templateUrl: './chart-list.component.html',
     styleUrls: ['./chart-list.component.css'],
     imports: [
-        HighchartsChartModule,
-        XmasComponent,
-        PinesChartComponent
-    ]
+    HighchartsChartModule,
+    XmasComponent,
+    PinesChartComponent,
+    PieChartComponent,
+]
 })
 export class ChartListComponent {}
